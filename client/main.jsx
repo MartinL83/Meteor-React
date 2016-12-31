87a3f0d32d90438adf7f8c912491b6c55c659817
Martin Lindgren <lindgren2d@gmail.com>
2016-12-31 13:32:06 +0100

    init

--- client/main.jsx
+++ client/main.jsx
@@ -1,1 +1,9 @@
 import '/imports/startup/client/index.jsx';
+
+// Import Tether.js - Bootstrap tooltip requires Tether.
+// We also add 'global.Tether' as a workaround for Meteor.
+import tether from 'tether';
+global.Tether = tether;
+
+// Import Bootstrap JS.
+bootstrap = require('bootstrap');
--- imports/ui/components/test.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const Welcome = ({name}) => (
-    <div>
-        Hello, {name}.
-    </div>
-);
